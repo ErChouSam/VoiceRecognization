b796e7cc9d7fa4296819f396fc682600143367e0
ErChouSam <samy.yousfi02@laposte.net>
2017-09-18 15:01:31 +0200

    Fist Commit
    First commit with test interface

---
+++ VoiceRecognization/Choice.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace VoiceRecognization
+{
+    class Choice
+    {
+        int CommandCode = 0;
+        int retry = 0;
+        SpeechRecognitionEngine SREngineChoice = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine SREngineConfirm = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine SREngineStart = new SpeechRecognitionEngine();
+        SpeechSynthesizer Synthesizer = new SpeechSynthesizer();
+
+        private void init()
+        {
+            Choices choice = new Choices();
+            Choices confirm = new Choices();
+            Choices start = new Choices();
+            start.Add(new String[] { "Nova", "Hey Nova" });
+            confirm.Add(new String[] { "Oui", "Non" });
+            choice.Add(new String[] { "ouvre opéra", " quelle est mon nom", "Yes" });
+            GrammarBuilder ConfirmgBuilder = new GrammarBuilder(confirm);
+            GrammarBuilder ChoicegBuilder = new GrammarBuilder(choice);
+            GrammarBuilder StartgBuilder = new GrammarBuilder(start);
+            Grammar StartGrammar = new Grammar(StartgBuilder);
+            Grammar ChoiceGrammar = new Grammar(ChoicegBuilder);
+            Grammar ConfirmGrammar = new Grammar(ConfirmgBuilder);
+
+            SREngineStart.LoadGrammarAsync(StartGrammar);
+            SREngineStart.SetInputToDefaultAudioDevice();
+            SREngineStart.SpeechRecognized += SREngineStart_SpeechRecognized;
+
+            SREngineChoice.LoadGrammarAsync(ChoiceGrammar);
+            SREngineChoice.SetInputToDefaultAudioDevice();
+            SREngineChoice.SpeechRecognized += SREngineChoice_SpeechRecognized;
+
+            SREngineConfirm.LoadGrammar(ConfirmGrammar);
+            SREngineConfirm.SetInputToDefaultAudioDevice();
+            SREngineConfirm.SpeechRecognized += SREngineConfirm_SpeechRecognized;
+        }
+
+        void SREngineStart_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Nova":
+                case "Hey Nova":
+                    retry = 0;
+                    Synthesizer.SpeakAsync("En quoi puis-je vous aider?");
+                    SREngineStart.RecognizeAsyncCancel();
+                    SREngineChoice.RecognizeAsync(RecognizeMode.Multiple);
+                    TimeSet.Start();
+                    break;
+            }
+        }
+
+        void SREngineConfirm_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Oui":
+                    SREngineConfirm.RecognizeAsyncCancel();
+                    if (CommandCode == 1)
+                        System.Diagnostics.Process.Start("C:\\Program Files (x86)\\Opera\\launcher.exe");
+                    else
+                        MessageBox.Show("Erreur");
+                    SREngineStart.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "Non":
+                    SREngineConfirm.RecognizeAsyncCancel();
+                    Synthesizer.SpeakAsync("Opera ne se lancera pas");
+                    SREngineStart.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+            }
+
+        }
+
+        void SREngineChoice_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "ouvre opéra":
+                    SREngineChoice.RecognizeAsyncCancel();
+                    CommandCode = 1;
+                    Synthesizer.SpeakAsync("êtes-vous sur?");
+                    SREngineConfirm.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "quelle est mon nom":
+                    rtbLog.Text += "\nSamy";
+                    break;
+                default:
+                    Synthesizer.SpeakAsync("Je n'ai pas compris votre demande");
+                    break;
+            }
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            SREngineStart.RecognizeAsync(RecognizeMode.Multiple);
+            btDisable.Enabled = true;
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            SREngineChoice.RecognizeAsyncCancel();
+            SREngineStart.RecognizeAsyncCancel();
+            SREngineConfirm.RecognizeAsyncCancel();
+            btDisable.Enabled = false;
+        }
+        private void TimerOut(object sender, EventArgs e)
+        {
+            if (retry == 2)
+            {
+                TimeSet.Stop();
+                SREngineChoice.RecognizeAsyncCancel();
+                SREngineStart.RecognizeAsyncCancel();
+                Synthesizer.SpeakAsync("Je n'ai pas compris votre demande, je me rendors.");
+                SREngineStart.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+                Synthesizer.SpeakAsync("Je n'ai pas compris votre demande, réessayer.");
+            retry++;
+
+        }
+    }
+}
---
+++ VoiceRecognization/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace VoiceRecognization
+{
+    partial class Nova
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btEnable = new System.Windows.Forms.Button();
+            this.btDisable = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.TimeSet = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btEnable
+            // 
+            this.btEnable.Location = new System.Drawing.Point(12, 282);
+            this.btEnable.Name = "btEnable";
+            this.btEnable.Size = new System.Drawing.Size(155, 23);
+            this.btEnable.TabIndex = 0;
+            this.btEnable.Text = "Enable Vocal";
+            this.btEnable.UseVisualStyleBackColor = true;
+            // 
+            // btDisable
+            // 
+            this.btDisable.Location = new System.Drawing.Point(600, 282);
+            this.btDisable.Name = "btDisable";
+            this.btDisable.Size = new System.Drawing.Size(173, 23);
+            this.btDisable.TabIndex = 1;
+            this.btDisable.Text = "Disable Vocal";
+            this.btDisable.UseVisualStyleBackColor = true;
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.rtbLog.Location = new System.Drawing.Point(12, 12);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(761, 264);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "-log-";
+            // 
+            // Nova
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 317);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.btDisable);
+            this.Controls.Add(this.btEnable);
+            this.Name = "Nova";
+            this.Text = "Nova";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btEnable;
+        private System.Windows.Forms.Button btDisable;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Timer TimeSet;
+    }
+}
+
